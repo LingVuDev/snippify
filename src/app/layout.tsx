c93f2250437ac700e1104dee87ec47872048d9e7
Ling Vu <m.vu@thalia.de>
2023-10-15 13:25:01 +0200

    Add additional sizes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,47 @@
-import React, { FC, PropsWithChildren } from 'react'
-import '@/styles/globals.css'
-import { Metadata } from 'next'
+import React, { FC, PropsWithChildren } from "react";
+import "@/styles/globals.css";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'snippify',
-  description: 'Discover & Share useful code snippets',
-}
+  title: "snippify",
+  description: "Discover & Share useful code snippets",
+};
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          sizes="any"
+          href="/favicon.ico"
+        />
       </head>
       <body>
-        <div className='main'>
-          <div className='gradient' />
+        <div className="main">
+          <div className="gradient" />
         </div>
-        <main className='app'>
-          {children}
-        </main>
+        <main className="app">{children}</main>
       </body>
-      </html>
-  )
-}
+    </html>
+  );
+};
 
 export default RootLayout;
